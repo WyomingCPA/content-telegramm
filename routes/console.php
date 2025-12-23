07d5edaa81e47f7e6f769ecf9048b08b8f0609b2
Nikolay <WyomingCPA@yandex.ru>
2025-12-23 13:58:19 +0300

    update shedule

--- routes/console.php
+++ routes/console.php
@@ -29,10 +29,10 @@ Schedule::command('app:get-post-photo-anime')->hourly();
 Schedule::command('command:check-is-file')->daily();
 
 //advert
-Schedule::command('app:create-sexy-ads')->daily();
-Schedule::command('app:create-anime-ads')->daily();
+Schedule::command('app:create-sexy-ads')->everySixHours($minutes = 40);
+Schedule::command('app:create-anime-ads')->everySixHours($minutes = 40);
 Schedule::command('app:create-advert-post')->everySixHours($minutes = 25);
 //Schedule::command('app:create-advert-tix-list-button')->daily();
 //$schedule->command('command:create-advert-list-button')->daily();
 
-Schedule::command('app:delete-ads')->everyThreeHours();
+Schedule::command('app:delete-ads')->hourly();
