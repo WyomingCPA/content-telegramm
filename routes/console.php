966513c46bcb196a71a2f7613d419e29455d5390
Nikolay <WyomingCPA@yandex.ru>
2026-04-07 10:21:49 +0300

    fix bug shedule

--- routes/console.php
+++ routes/console.php
@@ -9,33 +9,33 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 //vk
-Schedule::command('command:get-post-from-vk')->hourly();
-Schedule::command('command:delete-old-post')->everyTwoHours();
-Schedule::command('command:publish-anime-queue')->hourly();
-Schedule::command('command:publish-anime2-queue')->hourly()->sendOutputTo(storage_path('logs/publish-anime2-queue.log'));
-Schedule::command('command:publish-sexy-queue')->everyTwoHours();
+Schedule::command('command:get-post-from-vk')->hourly()->runInBackground()->withoutOverlapping();
+Schedule::command('command:delete-old-post')->everyTwoHours()->runInBackground()->withoutOverlapping();
+Schedule::command('command:publish-anime-queue')->hourly()->runInBackground()->withoutOverlapping();
+Schedule::command('command:publish-anime2-queue')->hourly()->runInBackground()->withoutOverlapping();
+Schedule::command('command:publish-sexy-queue')->everyTwoHours()->runInBackground()->withoutOverlapping();
 
 //tumblr 
-Schedule::command('command:queue-anime-photo-tumblr')->everyThreeHours();
-Schedule::command('app:get-post-from-tumblr-anime')->hourly();
+Schedule::command('command:queue-anime-photo-tumblr')->everyThreeHours()->runInBackground()->withoutOverlapping();
+Schedule::command('app:get-post-from-tumblr-anime')->hourly()->runInBackground()->withoutOverlapping();
 
 //telegram
-Schedule::command('app:get-post-from-telegramm-sexy')->hourly();
-Schedule::command('app:get-post-from-telegramm-sexy-video')->hourly();
-Schedule::command('app:get-post-photo-anime')->hourly();
+Schedule::command('app:get-post-from-telegramm-sexy')->hourly()->runInBackground()->withoutOverlapping();
+Schedule::command('app:get-post-from-telegramm-sexy-video')->hourly()->runInBackground()->withoutOverlapping();
+Schedule::command('app:get-post-photo-anime')->hourly()->runInBackground()->withoutOverlapping();
 
 //
-Schedule::command('app:chech-is-file')->daily();
+Schedule::command('app:chech-is-file')->daily()->runInBackground()->withoutOverlapping();
 
 //advert
-Schedule::command('app:create-sexy-ads')->daily();
-Schedule::command('app:create-anime-ads')->daily();
-Schedule::command('app:create-anime-addcit-advert')->everySixHours();
-Schedule::command('app:create-advert-post')->everySixHours();
+Schedule::command('app:create-sexy-ads')->daily()->runInBackground()->withoutOverlapping();
+Schedule::command('app:create-anime-ads')->daily()->runInBackground()->withoutOverlapping();
+Schedule::command('app:create-anime-addcit-advert')->everySixHours()->runInBackground()->withoutOverlapping();
+Schedule::command('app:create-advert-post')->everySixHours()->runInBackground()->withoutOverlapping();
 //Schedule::command('app:create-advert-tix-list-button')->daily();
 //$schedule->command('command:create-advert-list-button')->daily();
-Schedule::command('app:queue-sexy-video-telegramm')->everyTenMinutes();
-Schedule::command('app:queue-anime-photo-telegram')->everyFifteenMinutes();
-Schedule::command('app:queue-sexy-photo-telegram')->everyFifteenMinutes();
+Schedule::command('app:queue-sexy-video-telegramm')->everyTenMinutes()->runInBackground()->withoutOverlapping();
+Schedule::command('app:queue-anime-photo-telegram')->everyFifteenMinutes()->runInBackground()->withoutOverlapping();
+Schedule::command('app:queue-sexy-photo-telegram')->everyFifteenMinutes()->runInBackground()->withoutOverlapping();
 
-Schedule::command('app:delete-ads')->hourly();
+Schedule::command('app:delete-ads')->hourly()->runInBackground()->withoutOverlapping();
